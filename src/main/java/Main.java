9b03c3702e83570f49338116433b277169c31ca7
deanOfWalls <deanofwalls@gmail.com>
2023-04-17 23:43:43 -0400

    checkpointing work didnt get working 100% yet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+//import java.nio.file.*;import java.util.*;import java.util.regex.*;
+//public class Main{public static void main(String[]a)throws Exception{var r=Files.readString(Paths.get("src/main/resources/RawData.txt")).replace("^",";");var p=Pattern.compile("(?i)name:(\\w+);price:(\\d+\\.\\d{2});type:(\\w+);expiration:(\\d{1,2}/\\d{1,2}/\\d{4})");var t=new HashMap<String,Map<String,Integer>>();p.matcher(r).results().forEach(m->{var n=m.group(1).toLowerCase();t.computeIfAbsent(n,k->new HashMap<>()).merge(m.group(2),1,Integer::sum);});t.forEach((k,v)->{System.out.printf("name: %s seen: %d times%n============= \t =============%n",k,v.values().stream().mapToInt(Integer::intValue).sum());v.forEach((kk,vv)->System.out.printf("Price: \t%s\t seen: %d times%n-------------\t -------------%n",kk,vv));System.out.println();});System.out.printf("Errors \t\t seen: %d times",t.getOrDefault("",new HashMap<>()).getOrDefault("",0));}}
+//------
+
+
+
+
+
+
+
+
+
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
 import java.util.HashMap;
@@ -56,67 +78,3 @@ public class Main {
         System.out.println("Errors \t\tseen: " + itemsMap.getOrDefault("", new HashMap<>()).getOrDefault("", 0) + " times");
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//import org.apache.commons.io.IOUtils;
-//import java.io.IOException;
-//import java.util.HashMap;
-//import java.util.Map;
-//import java.util.regex.Matcher;
-//import java.util.regex.Pattern;
-//
-//public class Main {
-//
-//    public String readRawDataToString() throws IOException {
-//        ClassLoader classLoader = getClass().getClassLoader();
-//        String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
-//        result = result.replace("^", ";");
-//        return result;
-//    }
-//
-//    public static void main(String[] args) throws Exception {
-//        String rawData = (new Main()).readRawDataToString();
-//
-//        // Pattern to match each item in the data
-//        Pattern pattern = Pattern.compile("(?i)name:(\\w+);price:(\\d+\\.\\d{2});type:(\\w+);expiration:(\\d{1,2}/\\d{1,2}/\\d{4})");
-//
-//        // Map to store the results
-//        Map<String, Map<String, Integer>> itemsMap = new HashMap<>();
-//
-//        // Iterate over each match and add it to the map
-//        Matcher matcher = pattern.matcher(rawData);
-//        while (matcher.find()) {
-//            String name = matcher.group(1).toLowerCase();
-//            String price = matcher.group(2);
-//            if (!itemsMap.containsKey(name)) {
-//                itemsMap.put(name, new HashMap<>());
-//            }
-//            Map<String, Integer> itemData = itemsMap.get(name);
-//            itemData.put(price, itemData.getOrDefault(price, 0) + 1);
-//        }
-//
-//        // Print out the results
-//        for (Map.Entry<String, Map<String, Integer>> entry : itemsMap.entrySet()) {
-//            System.out.println("name: " + entry.getKey() + " seen: " + entry.getValue().values().stream().reduce(0, Integer::sum) + " times");
-//            System.out.println("============= \t =============");
-//            for (Map.Entry<String, Integer> priceEntry : entry.getValue().entrySet()) {
-//                System.out.println("Price: \t" + priceEntry.getKey() + "\t seen: " + priceEntry.getValue() + " times");
-//                System.out.println("-------------\t -------------");
-//            }
-//            System.out.println();
-//        }
-//        System.out.println("Errors \t\t seen: " + itemsMap.getOrDefault("", new HashMap<>()).getOrDefault("", 0) + " times");
-//    }
-//}
--- src/test/MainApplicationTest.java
+++ src/test/MainApplicationTest.java
@@ -1,6 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainApplicationTest {
@@ -12,7 +13,28 @@ public class MainApplicationTest {
         Assert.assertFalse(result.isEmpty());
     }
 
+    @Test
+    public void testItemPricesAreValid() throws Exception {
+        Main main = new Main();
+        String rawData = main.readRawDataToString();
+        Pattern pattern = Pattern.compile("(?i)price:(\\d+\\.\\d{2});");
+        Matcher matcher = pattern.matcher(rawData);
+        while (matcher.find()) {
+            String price = matcher.group(1);
+            Assert.assertTrue(Double.parseDouble(price) > 0);
+        }
+    }
 
-
+    @Test
+    public void testPatternMatch() throws Exception {
+        String input = "name:Milk;price:3.23;type:Food;expiration:1/25/2016";
+        Pattern pattern = Pattern.compile("(?i)name:([\\w\\s]+);price:(\\d+\\.\\d{2});type:(\\w+);expiration:(\\d{1,2}/\\d{1,2}/\\d{4})");
+        Matcher matcher = pattern.matcher(input);
+        Assert.assertTrue(matcher.find());
+        Assert.assertEquals("Milk", matcher.group(1));
+        Assert.assertEquals("3.23", matcher.group(2));
+        Assert.assertEquals("Food", matcher.group(3));
+        Assert.assertEquals("1/25/2016", matcher.group(4));
+    }
 
 }
