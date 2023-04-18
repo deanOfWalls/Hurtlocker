1b72207c4567f1527113d540252a03254e555017
deanOfWalls <deanofwalls@gmail.com>
2023-04-18 08:02:33 -0400

    fixed compile error that i missed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,46 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
 
+public class Main {
+    public static void main(String[] a) throws Exception {
+        // Read in the contents of the file "RawData.txt" as a string and replace any "^" characters with ";".
+        var r = Files.readString(Paths.get("src/main/resources/RawData.txt")).replace("^", ";");
 
+        // Define a regular expression pattern to match lines in the format "name:<name>;price:<price>;type:<type>;expiration:<expiration>".
+        var p = Pattern.compile("(?i)name:(\\w+);price:(\\d+\\.\\d{2});type:(\\w+);expiration:(\\d{1,2}/\\d{1,2}/\\d{4})");
 
+        // Create a new empty HashMap to hold the results.
+        var t = new HashMap<String, Map<String, Integer>>();
 
+        // Use the regular expression pattern to find matches in the input string and process them.
+        p.matcher(r).results().forEach(m -> {
+            // Extract the name from the match and convert it to lowercase.
+            var n = m.group(1).toLowerCase();
 
+            // Extract the price from the match and use it as a key in the HashMap. If the key doesn't exist, create a new entry with a value of 1. If it does exist, increment the value by 1.
+            t.computeIfAbsent(n, k -> new HashMap<>()).merge(m.group(2), 1, Integer::sum);
+        });
 
+        // Print out the results.
+        t.forEach((k, v) -> {
+            // Print out the name and the total number of times it was seen.
+            System.out.printf("name: %s seen: %d times%n============= \t =============%n", k, v.values().stream().mapToInt(Integer::intValue).sum());
 
+            // Print out each price and the number of times it was seen.
+            v.forEach((kk, vv) -> System.out.printf("Price: \t%s\t seen: %d times%n-------------\t -------------%n", kk, vv));
 
+            // Print a blank line to separate the output for each name.
+            System.out.println();
+        });
 
+        // Print out the number of times an error occurred (i.e., a line in the input file didn't match the regular expression pattern).
+        System.out.printf("Errors \t\t seen: %d times", t.getOrDefault("", new HashMap<>()).getOrDefault("", 0));
+    }
+}
 
-import java.nio.file.*;import java.util.*;import java.util.regex.*;
-public class Main{public static void main(String[]a)throws Exception{var r=Files.readString(Paths.get("src/main/resources/RawData.txt")).replace("^",";");var p=Pattern.compile("(?i)name:(\\w+);price:(\\d+\\.\\d{2});type:(\\w+);expiration:(\\d{1,2}/\\d{1,2}/\\d{4})");var t=new HashMap<String,Map<String,Integer>>();p.matcher(r).results().forEach(m->{var n=m.group(1).toLowerCase();t.computeIfAbsent(n,k->new HashMap<>()).merge(m.group(2),1,Integer::sum);});t.forEach((k,v)->{System.out.printf("name: %s seen: %d times%n============= \t =============%n",k,v.values().stream().mapToInt(Integer::intValue).sum());v.forEach((kk,vv)->System.out.printf("Price: \t%s\t seen: %d times%n-------------\t -------------%n",kk,vv));System.out.println();});System.out.printf("Errors \t\t seen: %d times",t.getOrDefault("",new HashMap<>()).getOrDefault("",0));}}
-------
 
 
 
